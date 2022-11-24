9ec761cc9f24343d2148267611ebf1a668eb74d9
Kai0so <kaioaugustoalmeida.m@gmail.com>
2022-11-24 16:14:29 -0300

    Cria a função scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,7 +38,29 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author a::text").get()
+    comments_count = selector.css("div.post-comments h5::text").get(default=0)
+    summary_selector = selector.css(
+        "div.entry-content > p:first-of-type *::text"
+    ).getall()
+    summary = "".join(summary_selector).strip()
+    tags = selector.css("section.post-tags a::text").getall()
+    category = selector.css("span.label::text").get()
+    dictionary = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
+    return dictionary
 
 
 # Requisito 5
