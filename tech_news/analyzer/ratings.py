c68323f5c38a221321b012e8081239e3ebc8c82f
Kai0so <kaioaugustoalmeida.m@gmail.com>
2022-11-29 18:42:59 -0300

    feat: Cria a função top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,19 @@
+from ..database import get_collection
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    search_result = []
+    news_result = (
+        get_collection()
+        .find()
+        .sort([("comments_count", -1), ("title", 1)])
+        .limit(5)
+    )
+    for news in news_result:
+        news_data = (news["title"], news["url"])
+        search_result.append(news_data)
+    return search_result
 
 
 # Requisito 11
