a57508deabdbd278344c7595cdb0efd8d34e22c4
Kai0so <kaioaugustoalmeida.m@gmail.com>
2022-11-29 16:49:38 -0300

    feat: Cria a função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,19 @@
+from ..database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+    search_result = []
+    try:
+        news_result = search_news(query)
+    except FileNotFoundError:
+        return []
+    else:
+        for news in news_result:
+            news_data = (news["title"], news["url"])
+            search_result.append(news_data)
+        return search_result
 
 
 # Requisito 7
